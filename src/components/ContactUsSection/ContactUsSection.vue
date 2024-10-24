96ccb8217d5552010c4a35714ca8a21182899c3a
SerjoMhana <serjomh33@gmail.com>
2024-10-25 00:37:59 +0200

    Blogs

---
+++ src/components/ContactUsSection/ContactUsSection.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="h-[30rem] md:h-[calc(90vh-106px)]  flex flex-col bg-my-bg-image5 bg-cover bg-center bg-no-repeat"></div>
+<div class="max-w-7xl mx-auto px-4 xl:px-0 pb-10">
+    <div class="grid lg:grid-cols-7 lg:gap-x-12 xl:gap-x-12 lg:items-center">
+        <div class="lg:col-span-3 mt-10 lg:mt-0">
+            <img class="w-full rounded-xl" src="../../assets/products/اماكن-تواجد-فروعنا-.png" alt="Hero Image">
+        </div>
+        
+        <div class="lg:col-span-4">
+            <!-- Comment Form -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <div class="mx-auto max-w-2xl">
+    <div class="text-center">
+      <h2 class="text-xl text-gray-800 font-bold sm:text-3xl">
+        Contact Us
+      </h2>
+    </div>
+
+    <!-- Card -->
+    <div class="mt-5 p-4 relative z-10 bg-white border rounded-xl sm:mt-10 md:p-10">
+      <form>
+        <div class="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-name-1" class="block mb-2 text-sm font-medium">Full name</label>
+          <input type="text" id="hs-feedback-post-comment-name-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Full name">
+        </div>
+
+        <div class="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-email-1" class="block mb-2 text-sm font-medium">Email address</label>
+          <input type="email" id="hs-feedback-post-comment-email-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Email address">
+        </div>
+
+        <div>
+          <label for="hs-feedback-post-comment-textarea-1" class="block mb-2 text-sm font-medium">Comment</label>
+          <div class="mt-1">
+            <textarea id="hs-feedback-post-comment-textarea-1" name="hs-feedback-post-comment-textarea-1" rows="3" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Leave your comment here..."></textarea>
+          </div>
+        </div>
+
+        <div class="mt-6 grid">
+          <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Submit</button>
+        </div>
+      </form>
+    </div>
+    <!-- End Card -->
+  </div>
+</div>
+<!-- End Comment Form -->
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,12 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'my-bg-image': "url('@/assets/products/WhatsApp Image 2024-10-09 at 17.13.57_03dd14ea.jpg')",
-        'my-bg-image1': "url('@/assets/products/WhatsApp Image 2024-10-09 at 17.13.57_314c3234.jpg')",
-        'my-bg-image2': "url('@/assets/products/WhatsApp Image 2024-10-09 at 17.13.57_982e0caa.jpg')",
+        'my-bg-image': "url('@/assets/products/BANNER-2.png')",
+        'my-bg-image1': "url('@/assets/products/BANNER-3.png')",
+        'my-bg-image2': "url('@/assets/products/BANNER-4.png')",
         'my-bg-image3': "url('@/assets/products/photo1.jpg')",
         'my-bg-image4': "url('@/assets/products/12.png')",
+        'my-bg-image5': "url('@/assets/products/BANNER-1.png')",
       }}
   },
   plugins: [ require('flowbite/plugin'), require('preline/plugin'), ],
